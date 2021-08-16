a8d4276b64c4b5f00297fc7ee3bfc31a0b754496
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-16 23:55:42 +0800

    connected js file wiht html

---
+++ app.js
@@ -0,0 +1,6 @@
+var buyingPrice = document.querySelector('#buyingPrice');
+var quantity = document.querySelector('#quantity');
+var currentPrice = document.querySelector('currentPrice');
+var calculateButton = document.querySelector('#calculateButton');
+var outputResult = document.querySelector('#outputResult');
+
